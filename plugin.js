b891b9606f072e601020b12d388b3cbd177fd968
Rémi Becheras <rbecheras@sirap.fr>
2016-03-24 12:20:46 +0100

    remove unecessary code

--- plugin.js
+++ plugin.js
@@ -20,53 +20,14 @@ tinymce.PluginManager.add('forminputs', function(editor) {
     var inputElement = editor.dom.create('input', {type:'checkbox'});
     var labelElement = editor.dom.create('label', null, 'ma case à cocher');
 
-    console.log(labelElement);
-    console.log(inputElement);
 
     editor.dom.add(labelElement,inputElement);
     editor.dom.add(selection,labelElement);
 
-    tinymce.DOM.bind(inputElement,'click',function(){
-      alert('click');
-    });
-
   }
 
-  console.log('tinymce',tinymce);
-
-  jQuery("#frameName").contents().find(":checkbox").bind('click', function(){
-          val = this.checked; //<---
-          alert("changed");
-  });
-
-setTimeout(function(){
-  // console.log(editor.getDoc());
-  // tinymce.DOM.bind(document,'click',function(){
-  //   alert('click editor');
-  // });
-  var checkboxes = $(':checkbox',editor.getDoc());
-  console.log('checkboxes',checkboxes);
-  checkboxes.on('mouseover',function(){
-    var isOver = true;
-    var box = this;
-    $(box).on('mouseout',function(){
-      isOver = false;
     });
-    $(editor.getDoc()).on('click',function(){
-      if (isOver) {
-        console.log('checkbox clicked !');
-      }
-    });
-  });
-  checkboxes.on('focus',function(){
 
-  });
-},500);
-
-  editor.on('NodeChange',function(evt,a,b){
-    console.log('NodeChange');
-    console.log(evt,a,b);
-  });
 
   editor.addMenuItem('forminputs', {
     separator: 'before',
