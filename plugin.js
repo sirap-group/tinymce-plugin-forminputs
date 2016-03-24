eaa846d18af4671076c12055039b4f5ad435fa32
Rémi Becheras <rbecheras@sirap.fr>
2016-03-24 11:01:24 +0100

    save WIP

--- plugin.js
+++ plugin.js
@@ -13,7 +13,7 @@
 tinymce.PluginManager.add('forminputs', function(editor) {
 
   function insertCheckbox(evt){
-    console.log(evt);
+    // console.log(evt);
 
     var body = editor.getBody();
     var selection = editor.selection.getNode();
@@ -24,15 +24,49 @@ tinymce.PluginManager.add('forminputs', function(editor) {
     console.log(inputElement);
 
     editor.dom.add(labelElement,inputElement);
-    console.log(inputElement);
-
-    console.log(selection);
-
     editor.dom.add(selection,labelElement);
+
+    tinymce.DOM.bind(inputElement,'click',function(){
+      alert('click');
+    });
+
   }
 
+  console.log('tinymce',tinymce);
 
+  jQuery("#frameName").contents().find(":checkbox").bind('click', function(){
+          val = this.checked; //<---
+          alert("changed");
+  });
 
+setTimeout(function(){
+  // console.log(editor.getDoc());
+  // tinymce.DOM.bind(document,'click',function(){
+  //   alert('click editor');
+  // });
+  var checkboxes = $(':checkbox',editor.getDoc());
+  console.log('checkboxes',checkboxes);
+  checkboxes.on('mouseover',function(){
+    var isOver = true;
+    var box = this;
+    $(box).on('mouseout',function(){
+      isOver = false;
+    });
+    $(editor.getDoc()).on('click',function(){
+      if (isOver) {
+        console.log('checkbox clicked !');
+      }
+    });
+  });
+  checkboxes.on('focus',function(){
+
+  });
+},500);
+
+  editor.on('NodeChange',function(evt,a,b){
+    console.log('NodeChange');
+    console.log(evt,a,b);
+  });
 
   editor.addMenuItem('forminputs', {
     separator: 'before',
