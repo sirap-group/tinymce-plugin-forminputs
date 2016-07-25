e173d5cb0f4e01b4d9bcb168ed29e40d8ab70752
Rémi Becheras <rbecheras@sirap.fr>
2016-07-25 08:36:38 +0200

    format the whole plugin to the standard style guide

--- plugin.js
+++ plugin.js
@@ -8,102 +8,105 @@
  * Contributing: http://www.tinymce.com/contributing
  */
 
-/*global tinymce:true */
+(function (window) {
+  'use strict'
 
-tinymce.PluginManager.add('forminputs', function(editor) {
+  var prompt = window.prompt
+  var $ = window.$
+  var tinymce = window.tinymce
 
-  function insertCheckbox(evt){
-    var body = editor.getBody();
-    var selection = editor.selection.getNode();
-    var inputElement = editor.dom.create('input', {type:'checkbox'});
-    var labelElement = editor.dom.create('label', null, prompt('Saisir un label pour la case à chocher'));
+  tinymce.PluginManager.add('forminputs', function (editor) {
+    function insertCheckbox (evt) {
+      // var body = editor.getBody()
+      var selection = editor.selection.getNode()
+      var inputElement = editor.dom.create('input', {type: 'checkbox'})
+      var labelElement = editor.dom.create('label', null, prompt('Saisir un label pour la case à chocher'))
 
-    editor.dom.add(labelElement,inputElement);
-    editor.dom.add(selection,labelElement);
-    // editor.fire('change');
-    editor.nodeChanged();
-  }
-
-  function onCheckboxClick(evt){
-    evt.preventDefault();
-
-    console.log(this,evt);
-
-    var thisBox = $(this);
-    var toggle = !!thisBox.attr('checked');
-    var clone = thisBox.clone();
-    console.log('toggle',toggle,!toggle);
-
-
-    clone.attr('checked',!toggle);
-    clone.insertAfter(thisBox);
-    thisBox.remove();
-    editor.nodeChanged();
-    editor.fire('change');
-    editor.fire('SetContent');
-  }
-
-  function updateCheckboxesClickHandlers(){
-    $(':checkbox',editor.getDoc()).each(function(){
-      var box = this;
-      var parent = $(this).parent().get(0);
-      if (parent.tagName !== 'LABEL') {
-        parent = $('<label>');
-        $(box).wrap(parent);
-      }
-      $(parent).off('click').on('click',function(evt){
-        onCheckboxClick.call(box,evt);
-      });
-    });
-  }
-
-  var CallOnceOnTimeoutFactory = (function(){
-    function CallOnceOnTimeoutFactory(timeout,updateFunction){
-      this.timeout = timeout;
-      this.launched = false;
-      this.callback = null;
-
-      // set or update the callback but don't update it if `updateFunction` is set to false.
-      if (updateFunction === undefined) this.updateFunction = true;
-      else this.updateFunction = updateFunction;
-
-      console.log('this.updateFunction',this.updateFunction);
+      editor.dom.add(labelElement, inputElement)
+      editor.dom.add(selection, labelElement)
+      // editor.fire('change')
+      editor.nodeChanged()
     }
-    CallOnceOnTimeoutFactory.prototype.updateCallback = function(callback){
-      this.callback = callback;
-    };
-    CallOnceOnTimeoutFactory.prototype.callCallback = function(){
-      this.launched = false;
-      this.callback();
-    };
-    CallOnceOnTimeoutFactory.prototype.callOnce = function(syncFn){
-      var that = this;
-      if (this.updateFunction || this.callback === null) this.updateCallback(syncFn);
-      if (!this.launched) {
-        this.launched = true;
-        setTimeout(function(){
-          that.callCallback();
-        },this.timeout);
+
+    function onCheckboxClick (evt) {
+      evt.preventDefault()
+
+      console.log(this, evt)
+
+      var thisBox = $(this)
+      var toggle = !!thisBox.attr('checked')
+      var clone = thisBox.clone()
+      console.log('toggle', toggle, !toggle)
+
+      clone.attr('checked', !toggle)
+      clone.insertAfter(thisBox)
+      thisBox.remove()
+      editor.nodeChanged()
+      editor.fire('change')
+      editor.fire('SetContent')
+    }
+
+    function updateCheckboxesClickHandlers () {
+      $(':checkbox', editor.getDoc()).each(function () {
+        var box = this
+        var parent = $(this).parent().get(0)
+        if (parent.tagName !== 'LABEL') {
+          parent = $('<label>')
+          $(box).wrap(parent)
+        }
+        $(parent).off('click').on('click', function (evt) {
+          onCheckboxClick.call(box, evt)
+        })
+      })
+    }
+
+    var CallOnceOnTimeoutFactory = (function () {
+      function CallOnceOnTimeoutFactory (timeout, updateFunction) {
+        this.timeout = timeout
+        this.launched = false
+        this.callback = null
+
+        // set or update the callback but don't update it if `updateFunction` is set to false.
+        if (updateFunction === undefined) this.updateFunction = true
+        else this.updateFunction = updateFunction
+
+        console.log('this.updateFunction', this.updateFunction)
       }
-    };
-    return CallOnceOnTimeoutFactory;
-  })();
+      CallOnceOnTimeoutFactory.prototype.updateCallback = function (callback) {
+        this.callback = callback
+      }
+      CallOnceOnTimeoutFactory.prototype.callCallback = function () {
+        this.launched = false
+        this.callback()
+      }
+      CallOnceOnTimeoutFactory.prototype.callOnce = function (syncFn) {
+        var that = this
+        if (this.updateFunction || this.callback === null) this.updateCallback(syncFn)
+        if (!this.launched) {
+          this.launched = true
+          setTimeout(function () {
+            that.callCallback()
+          }, this.timeout)
+        }
+      }
+      return CallOnceOnTimeoutFactory
+    })()
 
-  var callOnceUpdateCheckboxesClickHandler = new CallOnceOnTimeoutFactory(150);
+    var callOnceUpdateCheckboxesClickHandler = new CallOnceOnTimeoutFactory(150)
 
-  editor.on('init NodeChange change SetContent', function(){
-    callOnceUpdateCheckboxesClickHandler.callOnce( updateCheckboxesClickHandlers );
-  });
+    editor.on('init NodeChange change SetContent', function () {
+      callOnceUpdateCheckboxesClickHandler.callOnce(updateCheckboxesClickHandlers)
+    })
 
-  editor.addMenuItem('forminputs', {
-    separator: 'before',
-    text: 'Form',
-    context: 'insert',
-    menu: [{
-      text: 'Checkbox',
-      onclick: insertCheckbox,
-      onPostRender: null
-    }]
-  });
-
-});
+    editor.addMenuItem('forminputs', {
+      separator: 'before',
+      text: 'Form',
+      context: 'insert',
+      menu: [{
+        text: 'Checkbox',
+        onclick: insertCheckbox,
+        onPostRender: null
+      }]
+    })
+  })
+})(window)
